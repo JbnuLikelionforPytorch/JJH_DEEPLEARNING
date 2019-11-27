bcfcdb6da5076acca122a3fb60f4edac7dc2fd7e
citrussoda <dndhkaus1004@naver.com>
2019-11-27 22:49:34 +0900

    Feat: Add backpropagation of Sigmoid example

---
+++ 1-Chap.5/backpg_sigmoid.py
@@ -0,0 +1,17 @@
+import numpy as np
+
+
+class Sigmoid:
+    def __init__(self):
+        self.out = None
+
+    def foward(self, x):
+        out = 1 / (1 + np.exp(-x))
+        self.out = out
+
+        return out
+
+    def backward(self, dout):
+        dx = dout * self.out * (1 - self.out)
+
+        return dx
